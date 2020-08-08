55866c7782a088b0efea14fc0be7ead7461a1f92
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2020-08-07 21:32:26 -0300

    Substituindo variavel temporaria por consulta

--- chapter-1.js
+++ chapter-1.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
 
     let thisAmount = amountFor(perf, play);
 
@@ -31,6 +31,10 @@ function statement(invoice, plays) {
   result += `Amount owned is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
 }
 
 function amountFor(aPerformance, play) {
