5d9063320b0d5fa1f3430a62c930fab9bc5fa2d6
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2020-08-08 13:52:47 -0300

    Removendo parametro invoice substituindo por objeto statementData

--- chapter-1.js
+++ chapter-1.js
@@ -2,33 +2,35 @@ const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-  for (let perf of invoice.performances) {
-    // exibe a linha para esta requisição
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
   }
 
   result += `Amount owned is ${usd(totalAmount() / 100)}\n`;
-  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoices.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
   }
 
-  function totalVolumeCredits(invoice) {
+  function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
