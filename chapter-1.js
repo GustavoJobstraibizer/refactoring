5824463ba1e5510c3a3a8a585df28df39907ba43
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2020-08-18 22:29:28 -0300

    Extraindo para metodo statement data create

--- chapter-1.js
+++ chapter-1.js
@@ -2,26 +2,21 @@ const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -63,6 +58,8 @@ function statement(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+
+  return statementData;
 }
 
 function renderPlainText(data, plays) {
